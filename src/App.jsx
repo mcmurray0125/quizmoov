df83b4fe704649e1aaf6244b292dff9a785a89b3
Michael Murray <mcmurray0125@gmail.com>
2023-02-13 20:47:23 -0600

    created quiz state to store all quiz information, like the correct answer, multiple choice options, uid, and if the quiz is correct or not.

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,70 @@
 import { useState, useEffect } from "react"
+import { nanoid } from "nanoid"
 import movieQuotes from "movie-quotes"
 import { Container } from "react-bootstrap"
 import Navigation from "./Navigation"
 
 function App() {
 const [allQuotes, setAllQuotes] = useState(movieQuotes.all) //movieQuotes.all returns an Array of 100 quotes + titles.
-const [fourQuotes, setFourQuotes] = useState([])
 const [options, setOptions] = useState([])
-const [chosenMovie, setChosenMovie] = useState("")
-const [chosenQuote, setChosenQuote] = useState("")
-const [scrambledOptions, setScrambledOptions] = useState([])
+const [correctMovie, setCorrectMovie] = useState("")
+const [correctQuote, setCorrectQuote] = useState("")
+const [quiz, setQuiz] = useState ({})
 
-//Select 4 random Quotes + titles. Picks one as the chosen answer.
 useEffect(() => {
   let selectedQuotes = []
   for (let i = 0; i < 4; i++) {
     let randomQuotes = Math.floor(Math.random() * allQuotes.length);
     selectedQuotes.push(allQuotes[randomQuotes]);
   }
-  console.log(selectedQuotes)
-  setFourQuotes(selectedQuotes)
-  setChosenMovie(selectedQuotes[0])
-},[])
+  setCorrectMovie(selectedQuotes[0])
 
-//Sets options to 4 movie titles. Quotes are removed from strings.
-useEffect(() => {
   let movieOptions = []
-  fourQuotes.forEach(function (item) {
+  selectedQuotes.forEach(function (item) {
     let substrings = item.split('" ');
     movieOptions.push(substrings[substrings.length - 1])
   })
-  //Scrambles the options.
+
   movieOptions.sort(() => Math.random() - 0.5);
   setOptions(movieOptions)
-},[fourQuotes])
+}, [])
+
 
 
 //Splits the chosen string to only display the quote. Title is removed from string.
 useEffect(() => {
-  let str = chosenMovie;
+  let str = correctMovie;
   let lastIndex = str.lastIndexOf('" ');
-  setChosenQuote(str.substring(0, lastIndex + 2))
-},[chosenMovie])
-
+  setCorrectQuote(str.substring(0, lastIndex + 2))
+},[correctMovie])
 
 useEffect(() => {
-console.log(chosenMovie)
 console.log(options)
-console.log(chosenQuote)
-},[options, fourQuotes, chosenQuote])
+console.log(correctQuote)
+console.log(quiz)
+},[quiz])
 
+useEffect(() => {
+  setQuiz({
+    correctMovie: correctMovie,
+    correctQuote: correctQuote,
+    options: options.map((option, index) => {
+      return {
+        title: option,
+        guessed: false,
+      };
+    }),
+    id: nanoid(),
+    correct: false,
+  });
+}, [correctQuote]);
 
   return (
     <>
       <Navigation/>
       <Container className="text-center mt-5">
-        <h2>{chosenQuote}</h2>
+        <h4 className="mb-5">What movie is this quote from?</h4>
+        <h3>{correctQuote}</h3>
       </Container>
     </>
   )
